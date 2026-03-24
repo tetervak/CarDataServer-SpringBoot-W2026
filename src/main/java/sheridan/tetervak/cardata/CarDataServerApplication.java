790ca7a283248147de648c894103eb4dfd3a618e
Alexander Tetervak <tetervak@sheridancollege.ca>
2026-03-24 10:36:34 -0400

    fresh project

---
+++ src/main/java/sheridan/tetervak/cardata/CarDataServerApplication.java
@@ -0,0 +1,13 @@
+package sheridan.tetervak.cardata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDataServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarDataServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/tetervak/cardata/CarDataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.tetervak.cardata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDataServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
